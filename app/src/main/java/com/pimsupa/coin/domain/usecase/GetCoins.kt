87bc95b07eb084590eaaae7edb67bab3d65011d7
Pimsupa.chi <pimsupa.chi@axonstech.com>
2024-07-06 11:37:26 +0700

    update usecase using flow instead of Result

--- app/src/main/java/com/pimsupa/coin/domain/usecase/GetCoins.kt
+++ app/src/main/java/com/pimsupa/coin/domain/usecase/GetCoins.kt
@@ -7,27 +7,33 @@ import com.pimsupa.coin.util.CoinDispatchers
 import com.pimsupa.coin.util.CoinException
 import com.pimsupa.coin.util.Dispatcher
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 interface GetCoins {
-    suspend operator fun invoke(page: Int): Result<List<Coin>>
+    operator fun invoke(page: Int): Flow<List<Coin>>
 }
 
 class GetCoinsImpl @Inject constructor(
     private val api: CoinApi,
     @Dispatcher(CoinDispatchers.IO) private val ioDispatcher: CoroutineDispatcher
 ) : GetCoins {
-    override suspend fun invoke(page: Int): Result<List<Coin>> = withContext(ioDispatcher) {
-        val response = api.getCoins(limit = 20, offset = page)
-        return@withContext if (response.isSuccessful) {
-            val data =
-                response.body()?.data
-                    ?: return@withContext Result.failure(CoinException.CoinIsNullException())
-            Result.success(data.coins.map { it.toCoin() })
-        } else {
-            Result.failure(CoinException.GetCoinsErrorException())
+    override fun invoke(page: Int): Flow<List<Coin>> = flow {
+        try {
+            val response = api.getCoins(limit = 20, offset = page)
+            if (response.isSuccessful) {
+                val data =
+                    response.body()?.data ?: throw CoinException.CoinIsNullException()
+                emit(data.coins.map { it.toCoin() })
+            } else {
+                throw CoinException.GetCoinsErrorException()
+            }
+        } catch (e: Exception) {
+            throw e
         }
-    }
+    }.flowOn(ioDispatcher)
 
 }
--- app/src/main/java/com/pimsupa/coin/ui/coinlist/CoinListViewModel.kt
+++ app/src/main/java/com/pimsupa/coin/ui/coinlist/CoinListViewModel.kt
@@ -4,13 +4,25 @@ import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.pimsupa.coin.domain.usecase.GetCoins
 import com.pimsupa.coin.util.BaseViewModel
+import com.pimsupa.coin.util.CoinDispatchers
 import com.pimsupa.coin.util.CoinException
+import com.pimsupa.coin.util.Dispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class CoinListViewModel @Inject constructor(private val getCoins: GetCoins) :
+class CoinListViewModel @Inject constructor(
+    private val getCoins: GetCoins,
+    @Dispatcher(CoinDispatchers.Default) private val defaultDispatcher: CoroutineDispatcher
+) :
     BaseViewModel<CoinListState>(CoinListState()) {
 
     private var currentPage = 0
@@ -25,27 +37,29 @@ class CoinListViewModel @Inject constructor(private val getCoins: GetCoins) :
 
     init {
         viewModelScope.launch {
-            setState {
-                copy(isLoading = true)
-            }
             getCoins()
         }
     }
 
     private suspend fun getCoins() {
         getCoins.invoke(currentPage)
-            .onSuccess {
-                val coinList = uiState.value.coins.toMutableList()
-                coinList.addAll(it)
+            .onStart {
+                setState { copy(isLoading = true) }
+            }
+            .onEach { data ->
+                val updatedCoins = withContext(defaultDispatcher) {
+                    val coinList = uiState.value.coins.toMutableList()
+                    coinList.addAll(data)
+                    coinList
+                }
                 setState {
                     copy(
-                        coins = coinList
+                        coins = updatedCoins
                     )
                 }
-                setState { copy(isLoading = false) }
             }
-            .onFailure {
-                when (it) {
+            .catch { exception ->
+                when (exception) {
                     is CoinException.CoinIsNullException, is CoinException.GetCoinsErrorException -> {
                         setState {
                             copy(
@@ -56,6 +70,10 @@ class CoinListViewModel @Inject constructor(private val getCoins: GetCoins) :
                     }
                 }
             }
+            .onCompletion {
+                setState { copy(isLoading = false) }
+            }
+            .collect()
     }
 
 
